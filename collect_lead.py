ee0e0977ab54456397d7e5c92cad3586438e33aa
Salman Kabir <sksshouvo2@gmail.com>
2024-01-04 11:23:34 +0600

    trying differently

--- collect_lead.py
+++ collect_lead.py
@@ -17,36 +17,17 @@ class collect_lead:
         else:
             driver = webdriver.Firefox()
             driver.get(link)
-            elements_array = driver.find_elements(By.CLASS_NAME, "hfpxzc")
-
-            for data in elements_array:
-                href = data.get_attribute('href')
-                print(href)
-                
-                # Click on the element to navigate to the link
-                if href:
-                     # Click on the element to open link in a new tab
-                    data.send_keys(Keys.CONTROL + Keys.RETURN)
-
-                    # Switch to the new tab
-                    driver.switch_to.window(driver.window_handles[1])
-                    print(driver.title)
-                    # Now you are on the newly opened tab
-                    # Extract information as needed
-                    # Example: Find and print the name, phone, and address
-                    name_element = driver.find_element(By.CSS_SELECTOR, ".DUwDvf.lfPIob")
-                    phone_element = driver.find_element(By.CSS_SELECTOR, ".Io6YTe.fontBodyMedium.kR99db")  # Replace with the actual CSS selector
-                    address_element = driver.find_element(By.CSS_SELECTOR, ".Io6YTe.fontBodyMedium.kR99db")  # Replace with the actual CSS selector
-
-                    print("Name:", name_element.text)
-                    print("Phone:", phone_element.text)
-                    print("Address:", address_element.text)
-
-                    # Close the new tab
-                    driver.close()
-
-                    # Switch back to the original tab
-                    driver.switch_to.window(driver.window_handles[0])
+            element = driver.find_element(By.CSS_SELECTOR, "a.hfpxzc")
+            href = element.get_attribute('href')
+            print("Link", href)
+            if href:
+                element.send_keys(Keys.CONTROL + Keys.RETURN)
+                driver.switch_to.window(driver.window_handles[1])
+                print(driver.current_url)
+                name_element = driver.find_element(By.TAG_NAME, 'h1')
+                print("Name:", name_element.text)
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
 
             time.sleep(10)
             driver.close()
